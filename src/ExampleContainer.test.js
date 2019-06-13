6b7076bb78922ae0023a98dab55f0998810636ff
Chris Weed <chris@cjweed.com>
2019-06-13 12:33:18 -0500

    Added the snapshot

--- src/ExampleContainer.test.js
+++ src/ExampleContainer.test.js
@@ -8,6 +8,18 @@ jest.mock('./ExampleView', () => props => <div data-id="ExampleView" {...props} 
 
 // So tests should test incoming data and validate outcoming data
 describe('ExampleContainer', () => {
+  test('should match snapshot', () => {
+    let result
+    act(() => {
+      result = renderer.create(<ExampleContainer iHaveMounted={() => {}} onSomething={() => {}} />)
+    })
+    // Snapshot test has a `<div data-id="ExampleView"`, so we aren't doing
+    // some excessive deep rendering when all we should be testing is this component
+    // react-test-renderer/shallow did not trigger the useEffect (below) so we use this
+    // technique to shallwo render but still trigger all the effects
+    expect(result).toMatchSnapshot()
+  })
+
   test('should call iHaveMounted on mount', () => {
     // This just tests to see if a prop is called on mount
     // In real life we may check some params sent back or validate that a listener
